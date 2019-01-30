3d0d7e89e83d558561ec1c821d9c0599d1cbf686
Souvik Basu <souvikbasu@gmail.com>
2019-01-30 13:11:37 +0530

    remove Header from Home page and move to App layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,12 +4,9 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import Header from "@/components/Header.vue"; // @ is an alias to /src
 
 @Component({
-  components: {
-    Header
-  }
+  components: {}
 })
 export default class Home extends Vue {}
 </script>
