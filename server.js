c5d58ba193a92af075ec3149fa1893d6ad6ee7b4
Souvik Basu <souvikbasu@gmail.com>
2019-02-03 10:42:48 +0530

    move express to dependencies instead of devDependencies

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require("express");
-const serveStatic = require("serve-static");
 const path = require("path");
-app = express();
-app.use(serveStatic(path.join(__dirname, "dist")));
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + "/dist"));
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname + "/dist/index.html"));
+});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
