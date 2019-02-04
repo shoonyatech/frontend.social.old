afc3cc8a5f2dbc5a58749823b6019b3f2a7b2b0a
Souvik Basu <souvikbasu@gmail.com>
2019-02-04 09:53:48 +0530

    create theme using element-variable.scss file

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
+import "../theme/index.css";
 import ElementUI from "element-ui";
-import "./element-variables.scss";
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
