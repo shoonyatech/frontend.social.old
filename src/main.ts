6bddfefd3d69b6cec305da847e6e7e8ad3845043
Souvik Basu <souvikbasu@gmail.com>
2019-02-04 09:41:57 +0530

    add custome theme for element

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
+import ElementUI from "element-ui";
+import "./element-variables.scss";
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
+  render: h => h(App),
 }).$mount("#app");
