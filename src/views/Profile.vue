549e72959e1807451da5d90f07acfe6b632d4024
Souvik Basu <souvikbasu@gmail.com>
2019-04-07 09:18:16 +0530

    add profile page

---
+++ src/views/Profile.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="profile">Profile page</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {},
+})
+export default class Profile extends Vue {}
+</script>
