2146d8ecd5292827de089949a30d8b3deddb6c57
Souvik Basu <souvikbasu@gmail.com>
2019-04-07 09:27:51 +0530

    add under contruction message in home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <div
+      class="msg"
+    >We are building a one stop platform for frontend developers and designers. Please check back in few days!</div>
+    <div class="msg">
+      If you want to contribute to this community effort, please email
+      <b>contact@shoonya.ca</b>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -10,3 +18,11 @@ import { Component, Vue } from "vue-property-decorator";
 })
 export default class Home extends Vue {}
 </script>
+
+<style scoped lang="scss">
+.msg {
+  margin: 50px 10px;
+  color: #aada18;
+  font-size: 28px;
+}
+</style>
