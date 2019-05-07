771d6ae283a321326a8cf6cb7eae86bbe926f684
mohitdutt <code3dworld@gmail.com>
2019-05-07 14:55:12 +0530

    get job page remove debuuger& add cmoments

--- src/views/Getjobs.vue
+++ src/views/Getjobs.vue
@@ -1,13 +1,15 @@
 <template>
+<!-- get job div -->
     <div class="getJobHeader">
         <div class="getJob mt-4">
             Jobs relevant to your skills
         </div>
         <div class="row mt-4" style="margin: 0;">
+            <!-- listing of all jobs -->
             <div class="col-md-10" id="menu">
                 <job-list v-for="joblist in this.$store.state.getAllJobs" :key="'job-id-' + joblist._id" :joblist="joblist"/>
             </div>
-
+            <!-- filter component for gt jobs -->
             <div class="col-md-2">
                 <filter-scope></filter-scope>
             </div>
@@ -16,39 +18,36 @@
 </template>
 
 <script>
-import Request from '@/services/Request'
-import JobList from '@/components/JobList'
-import FilterScope from '@/components/FilterScope'
+import Request from "@/services/Request";
+import JobList from "@/components/JobList";
+import FilterScope from "@/components/FilterScope";
 export default {
-    data() {
-        return {
-        }
-    },
     components:{
         JobList,
-        FilterScope
+        FilterScope,
     },
     created() {
-        this.processForm()
+        this.processForm();
     },
     methods: {
-        processForm(){
-            Request.getData('job').then((response)=>{
-              if(response.status === 200){
-                //   this.joblists =this.$store.state.getAllJobs = response.data
+        processForm(){ //get job api
+            Request.getData("job").then((response) => {
+              if( response.status === 200 ){
                   this.$store.state.getAllJobs = response.data
-                // alert('get job successfully!')
+                  alert("get job successfully!")
               }
-            }).catch((error)=>{
-              if(error.response.status == 500){
+            }).catch((error) => {
+              if( error.response.status === 500 ){
+                  alert("error")
               }
-            })
-        }
+            });
+        },
     },
-}
+};
 </script>
 
 <style scoped>
+/* get job css */
 .getJob{
     text-align: -webkit-auto;
     color: #000000;
@@ -58,14 +57,7 @@ export default {
 .getJobHeader{
     margin-left: 5%;
 }
-
-/* .vl {
-  border-left: 2px solid green;
-  height: 100px;
-} */
-#menu
-{
-    /* position: absolute; */
+#menu{
     border-right: 3px solid #aada20;
     top: 0;
     bottom: 0;
