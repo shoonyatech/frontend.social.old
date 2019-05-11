55b57bbb43494dbcc07a1937a2e4025cd910f875
mohitdutt <code3dworld@gmail.com>
2019-05-11 12:32:49 +0530

    more description file commit with remove dom manipulation

--- src/components/JobList.vue
+++ src/components/JobList.vue
@@ -1,97 +1,51 @@
 <template>
   <div>
+      <!-- joblisting div -->
     <div class="jobtitle">
         <span style="font-size:20px;">{{joblist.title}}</span><br><br>
-        <!-- <span>{{joblist.company}}</span>
-        <span v-for="jobskill in joblist.skils">{{jobskill}}     </span><br>
-        <span>{{joblist.level}}</span> -->
         <div class="row">
             <div class="col-4">
                 <span>{{joblist.company}}</span>
             </div>
             <div class="col-4">
-                <span v-for="jobskill in joblist.skils">{{jobskill}}</span>
+                <span v-for="(jobskill,i) in joblist.skils" :key="i">{{jobskill+ '   '}} </span>
             </div>
             <div class="col-4">
                 <span>{{joblist.level}}</span>
             </div>
         </div><br>
-        <span class="jobdescription">{{joblist.description}}</span><br>
+        <div v-if="joblist.description.length<500">
+            <span class="jobdescription">{{joblist.description}}</span>
+        </div>
         
+        <div class="jobdescription" v-else-if="joblist.description.length>500">
+            <more-description :joblistDescription="joblist.description"></more-description><br>
+        </div>
         
-        <a style="font-size:20px" v-if="showmore" v-on:click="show_more()">show more</a>
-        <a style="font-size:20px" v-if="showless">show less</a>
     </div>
     <button class="btnDetails">Details</button>
-
+<!-- button to show further details about job -->
     <hr>
   </div>
 </template>
 
 <script>
+import MoreDescription from "@/components/MoreDescription";
 export default {
-    data() {
-        return {
-            showmore: false,
-            showless: false,
-            hiddencontent: ''
-
-        }
+    components:{
+        MoreDescription
     },
     props: {
-        joblist: {
+        joblist: { //joblist object coming from get jobs
           type: Object,
           default: null
         }
-    },
-    created() {
-        
-    },
-    mounted() {
-        
-        if(this.joblist.description.length>512){
-            var showChar = 512;
-            var ellipsestext = "...";
-            var moretext = "moreclick";
-            var lesstext = "less";
-            var showcontent = this.joblist.description.substr(0, showChar)+ ' .....';
-            var hiddencontent = this.joblist.description.substr(showChar-1, this.joblist.description.length - showChar);
-            var html = showcontent + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent><span display: none;>'
-                         + hiddencontent + '</span>&nbsp;&nbsp;<a ref="mydiv" href="" class="morelink">' + moretext + '</a></span>';
-            // this.showmore = true 
-            // this.joblist.description = html
-            this.$el.getElementsByClassName('jobdescription')[0].innerHTML = html
-            // this.showmore = true
-        }
-    //     this.$refs['mydiv'].addEventListener('click', function(event) {
-    // //         event.preventDefault();
-    // //   console.log('clicked: ', event.target);
-    //     })
-
-        // $(".morelink").click(function(){
-        //     if($(this).hasClass("less")) {
-        //         $(this).removeClass("less");
-        //         $(this).html(moretext);
-        //     } else {   
-        //         $(this).addClass("less");
-        //         $(this).html(lesstext);
-        //     }
-        //     $(this).parent().prev().toggle();
-        //     $(this).prev().toggle();
-        //     return false;
-        // });
-    },
-
-    methods: {
-        show_more(){
-            this.joblist.description =this.joblist.description
-        },
-        asd(){alert()}
     },
 }
 </script>
 
 <style scoped>
+/* job listing style */
     .jobtitle{
         text-align: justify;
         color: #aada20;
@@ -108,6 +62,10 @@ export default {
     hr{
         margin-top: 50px;
         border-top: 1px solid #aada20;
+    }
+    .morecontent, .lesscontent{
+        cursor: pointer;
+        color: #638604;
     }
 </style>
 
