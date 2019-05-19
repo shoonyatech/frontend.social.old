4068102b2ee7588d65d4158ebc3cbf6da0337599
mohitdutt <code3dworld@gmail.com>
2019-05-19 22:50:44 +0530

    chaneg service to .js from .ts

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,8 @@
-import axios from "axios";
+import axios from 'axios'
 
-export default() => {
+export default() =>{
     return axios.create({
       baseURL: `https://frontend-social-api.herokuapp.com/`
-    });
+    })
   }
+  
