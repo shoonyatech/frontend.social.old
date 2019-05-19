5a82a3aabbe2205810aebef19e5df9005633a3b2
mohitdutt <code3dworld@gmail.com>
2019-05-19 14:10:09 +0530

    city search page get devloper info + past and incoming confernce get api done

---
+++ src/components/UpcomingConference.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="row mt-1">
+        <div class="col-4 textcolorgreen"><span>{{upConf.name}}</span></div>
+        <div class="col-5 textcolorgreen"><span>{{this.convertfromDate(upConf.dateFrom)}}</span></div>
+        <div class="col-3"><button class="DetailButton textcolorgreen">Details</button></div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    props: {
+        upConf: { // Conference list object coming from get jobs
+          type: Object,
+          default: null
+        }
+    },
+    created() {
+        this.convertfromDate(this.upConf.dateFrom)
+    },
+    methods: {
+        convertfromDate(str) {
+            var date = new Date(str),
+            mnth = ("0" + (date.getMonth()+1)).slice(-2),
+            day  = ("0" + date.getDate()).slice(-2);
+            return [ date.getFullYear(), mnth, day ].join("-");
+        },
+    }
+}
+</script>
+
+<style scoped>
+.textcolorgreen{
+  color: #aada20;
+  font-size: 21px;
+}
+.DetailButton{
+    background: none;
+    border: none;
+    border-bottom: 1px solid #aada20;
+}
+</style>
