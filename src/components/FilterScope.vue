399beca2ad451c24453134b9c8012b588ba65e86
mohitdutt <code3dworld@gmail.com>
2019-05-25 14:27:45 +0530

    getjobs mobile view design completed

--- src/components/FilterScope.vue
+++ src/components/FilterScope.vue
@@ -5,24 +5,24 @@
             <input class="searchdiv" placeholder= "Search..." :value="searchtext" @input="updateSearch">
         </div>
         <div class="row skills mt-5">
-            <div class="col-12 col-md-12 col-sm-1">
+            <div class="">
                  <b-form-group>
                     <b-form-checkbox-group class="skillscheckbox textcolorgreen" v-model="selectedSkils" name="flavour-2a" stacked v-on:change="updateSkills">
-                        <b-form-checkbox class="col-md-2 webkitbox" value="React">React</b-form-checkbox>
-                        <b-form-checkbox class="col-md-2 webkitbox" value="Angular">Angular</b-form-checkbox>
-                        <b-form-checkbox class="col-md-2 webkitbox" value="Vue">Vue</b-form-checkbox>
-                        <b-form-checkbox class="webkitbox" value="Web Components">Web Components</b-form-checkbox>
+                        <b-form-checkbox class="webkitbox" value="React">React</b-form-checkbox>
+                        <b-form-checkbox class="webkitbox" value="Angular">Angular</b-form-checkbox>
+                        <b-form-checkbox class="webkitbox" value="Vue">Vue</b-form-checkbox>
+                        <b-form-checkbox class="webcomponent" value="Web Components">Web Components</b-form-checkbox>
                     </b-form-checkbox-group>
                  </b-form-group>
             </div>
         </div>
 
         <div class="row mt-5 ml-0 timeStatus">
-            <b-form-checkbox class="col-md-6 textcolorgreen webkitboxStatus" v-model ="fulltimestatus" name="fulltimestatus" v-on:change="updateFulltime">
+            <b-form-checkbox class="col-md-6 times textcolorgreen webkitboxStatus" v-model ="fulltimestatus" name="fulltimestatus" v-on:change="updateFulltime">
               <span>Full time</span>
             </b-form-checkbox>
             
-            <b-form-checkbox class="col-md-6 textcolorgreen webkitboxStatus" v-model ="parttimestatus" name="parttimestatus" v-on:change="updateParttime">
+            <b-form-checkbox class="col-md-6 times textcolorgreen webkitboxStatus" v-model ="parttimestatus" name="parttimestatus" v-on:change="updateParttime">
               <span>Part time</span>
             </b-form-checkbox>
         </div>
@@ -93,6 +93,7 @@ export default {
 
 <style scoped>
 /* filter style */
+    
     .searchdiv{
         border: 2px solid #aada20;
         width:100%
@@ -117,6 +118,10 @@ export default {
     .webkitbox, .webkitboxStatus{
         display: -webkit-box;
     }
+
+    .webcomponent{
+        display: inline;
+    }
     
     /* media query  for filters*/
     @media (max-width: 767px) {
@@ -129,6 +134,13 @@ export default {
         .webkitboxStatus{
           flex: 0 0 100%;
           max-width: 100%;
+        }
+    }
+
+    @media (min-width: 1322px) and (max-width: 1417px) {
+        .times{
+          flex: 0 0 75%;
+          max-width: 75%;
         }
     }
 </style>
