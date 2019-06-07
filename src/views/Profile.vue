63af7ca40d8878986b038b7db8bc92349def7a67
mohitdutt <code3dworld@gmail.com>
2019-06-08 03:36:30 +0530

    update user detail in profile page done except userimage

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -3,22 +3,37 @@
     <div class="top-section">
       <div class="profile-pic">
         <img v-bind:src="getuserDetails.profilePic">
-        <div class="username">{{ getuserDetails.name }}</div>
+        <input type="text" ref="user_name" :value="getuserDetails.name" :disabled="!isEditingUsername"
+           :class="{view: !isEditingUsername}" class="nameInput mt-1">
+
+        <button @click="isEditingUsername = !isEditing" v-if="!isEditingUsername">Edit</button>
+        <button @click="saveUsername" v-else-if="isEditingUsername">Save</button>
+        <button class="ml-2" v-if="isEditingUsername" @click="isEditingUsername = false">Cancel</button>
       </div>
       
-      <div class="social-links mt-5">
+      <div class="social-links">
         <div class="" v-for="socials in getuserDetails.social" :key="'socials-id' + socials">
-        <LabelValue :label=socials class="fontlabel" />
+
+          <input type="text" :ref="socials" :value="socials" :disabled="!isEditingSocial"
+           :class="{view: !isEditingSocial}" class="socialInput mt-2">
         </div>
+        <button class="socialbtn" @click="isEditingSocial = !isEditingSocial" v-if="!isEditingSocial">Edit</button>
+        <button class="socialbtn" @click="saveSocial" v-else-if="isEditingSocial">Save</button>
+        <button class="socialbtn ml-2" v-if="isEditingSocial" @click="isEditingSocial = false">Cancel</button>
+        
       </div>
     </div>
-    <!-- <div>{{ user.info }}</div> -->
     <div class="skills-section">
       <div class="row" v-for="skill in getuserDetails.skills" :key="'abc-id' + skill">
         <div class="col-md-2">
-          <span>{{skill}}</span>
+          <input type="text" :ref="skill" :value="skill" :disabled="!isEditingSkill"
+           :class="{view: !isEditingSkill}" class="skillInput mt-2">
         </div>
       </div>
+
+      <button @click="isEditingSkill = !isEditingSkill" v-if="!isEditingSkill">Edit</button>
+      <button @click="saveSkill" v-else-if="isEditingSkill">Save</button>
+      <button class="ml-2" v-if="isEditingSkill" @click="isEditingSkill = false">Cancel</button>
     </div>
     <div class="conf-section mt-5">
       <div class="row">
@@ -28,7 +43,10 @@
               <span>Conferences Attended</span>
             </div>
             <div class="col-md-6">
-              <span class="fontmatch" v-for="confAtnd in getuserDetails.confAttended" :key="'abc-id' + confAtnd"> {{confAtnd}},</span>
+              <input type="text" :ref="confAtnd" :value="confAtnd+','" :disabled="!isEditingMeets"
+                v-for="confAtnd in getuserDetails.confAttended" 
+                :key="'abc-id' + confAtnd"
+                :class="{view: !isEditingMeets}" class="meetupInput">
             </div>
           </div>
         </div>
@@ -38,7 +56,10 @@
               <span>Upcoming Conferences</span>
             </div>
             <div class="col-md-6">
-              <span class="fontmatch" v-for="confUpcmng in getuserDetails.confUpcoming" :key="'abc-id' + confUpcmng"> {{confUpcmng}},</span>
+              <input type="text" :ref="confUpcmng" :value="confUpcmng+','" :disabled="!isEditingMeets"
+                v-for="confUpcmng in getuserDetails.confUpcoming" 
+                :key="'abc-id' + confUpcmng"
+                :class="{view: !isEditingMeets}" class="meetupInput">
             </div>
           </div>
         </div>
@@ -51,7 +72,10 @@
               <span>Meetups Attended</span>
             </div>
             <div class="col-md-7">
-              <span class="fontmatch" v-for="meetupAtnd in getuserDetails.meetupAttended" :key="'abc-id' + meetupAtnd"> {{meetupAtnd}},</span>
+               <input type="text" :ref="meetupAtnd" :value="meetupAtnd+','" :disabled="!isEditingMeets"
+                  v-for="meetupAtnd in getuserDetails.meetupAttended" 
+                  :key="'abc-id' + meetupAtnd"
+                  :class="{view: !isEditingMeets}" class="meetupInput">
             </div>
           </div>
         </div>
@@ -61,10 +85,19 @@
               <span>Upcoming meetups</span>
             </div>
             <div class="col-md-7">
-              <span class="fontmatch" v-for="meetupUpcmng in getuserDetails.meetupUpcoming" :key="'abc-id' + meetupUpcmng"> {{meetupUpcmng}},</span>
+               <input type="text" :ref="meetupUpcmng" :value="meetupUpcmng+','" :disabled="!isEditingMeets"
+                  v-for="meetupUpcmng in getuserDetails.meetupUpcoming" 
+                  :key="'abc-id' + meetupUpcmng"
+                  :class="{view: !isEditingMeets}" class="meetupInput">
             </div>
           </div>
         </div>
+      </div>
+
+      <div class="row mt-3" style="display: -webkit-inline-box;">
+        <button @click="isEditingMeets = !isEditingMeets" v-if="!isEditingMeets">Edit</button>
+        <button @click="saveMeets" v-else-if="isEditingMeets">Save</button>
+        <button class="ml-2" v-if="isEditingMeets" @click="isEditingMeets = false">Cancel</button>
       </div>
     </div>
   </div>
@@ -74,11 +107,74 @@
 import { mapActions, mapState } from "vuex";
 import LabelValue from "./LabelValue.vue";
 export default {
+  data() {
+    return {
+      isEditing: false,
+      isEditingUsername: false,
+      isEditingSocial: false,
+      isEditingSkill: false,
+      isEditingMeets: false,
+    }
+  },
   computed: mapState([ // getting data from store
       "getuserDetails",
     ]),
   components: {
     LabelValue,
+  },
+  methods: {
+    saveUsername() {debugger
+      this.$store.state.getuserDetails.name = this.$refs['user_name'].value;
+      this.isEditingUsername = !this.isEditingUsername;
+
+      this.updateuserdetails();
+    },
+    saveSocial() {
+      let vim = this.$store.state.getuserDetails.social;
+      for (let i=0; i<vim.length; i++) {
+        vim[i] = this.$refs[vim[i]][0].value
+      }
+      this.isEditingSocial = !this.isEditingSocial;
+      
+      this.updateuserdetails();
+    },
+    saveSkill() {
+      let vim = this.$store.state.getuserDetails.skills;
+      for (let i=0; i<vim.length; i++) {
+        vim[i] = this.$refs[vim[i]][0].value
+      }
+      this.isEditingSkill = !this.isEditingSkill;
+
+      this.updateuserdetails();
+    },
+    saveMeets() {debugger
+      let vimA = this.$store.state.getuserDetails.confAttended;
+      let vimB = this.$store.state.getuserDetails.confUpcoming;
+      let vimC = this.$store.state.getuserDetails.meetupAttended;
+      let vimD = this.$store.state.getuserDetails.meetupUpcoming;
+      
+      for (let i=0; i<vimA.length; i++) {
+        vimA[i] = this.$refs[vimA[i]][0].value
+      }
+      for (let i=0; i<vimB.length; i++) {
+        vimB[i] = this.$refs[vimB[i]][0].value
+      }
+      for (let i=0; i<vimC.length; i++) {
+        vimC[i] = this.$refs[vimC[i]][0].value
+      }
+      for (let i=0; i<vimD.length; i++) {
+        vimD[i] = this.$refs[vimD[i]][0].value
+      }
+      this.isEditingMeets = !this.isEditingMeets;
+
+      this.updateuserdetails();
+    },
+
+    updateuserdetails() {debugger
+      if (localStorage.authToken) {
+        this.$store.dispatch("UPDATEUSERDETAILS", localStorage.getItem("authToken"));
+      }
+    },
   },
 };
 </script>
@@ -99,6 +195,7 @@ export default {
 
 .social-links {
   text-align: left;
+  margin-top: 4rem !important;
 }
 
 img{
@@ -113,6 +210,44 @@ img{
 .fontmatch{
   font-size: 20px;
   float: left;
+}
+
+.view {
+  border-color: transparent;
+  background-color: initial;
+  color: initial;
+  text-align: -webkit-auto;
+}
+
+.nameInput {
+  text-align: center;
+}
+
+.socialInput {
+  color: #aada18 !important;
+}
+
+.skillInput {
+  text-align: center;
+  color: #2c3e50;
+}
+
+button {
+    background: #fff;
+    border: 1px solid #aada18;
+    color: #aada18;
+    font-size: initial;
+}
+
+.socialbtn {
+  float: right;
+}
+
+.meetupInput {
+  width: 33%;
+  font-size: 17px;
+  color: #2c3e50;
+  /* float: left; */
 }
 
 @media (max-width: 768px) {
