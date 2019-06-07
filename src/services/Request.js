01961ffa1aa9d544cfe954044487cc2aa54b67b3
mohitdutt <code3dworld@gmail.com>
2019-06-07 12:50:48 +0530

    profile page bind data completed with change in design

--- src/services/Request.js
+++ src/services/Request.js
@@ -1,12 +1,13 @@
 import Api from "@/services/Api";
-console.log(Api)
 export default {
     postData(page, data) {
 
         return Api().post(page, data)
     },
     getData(page) {
-        console.log('d', Api);
         return Api().get(page)
+    },
+    getUserData(page, auth) {
+        return Api().get(page, auth)
     },
 }
